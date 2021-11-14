4dacc05e8ec08d656cb9d727889779481e4a7e35
Piyush kumar <iampiyushkr@gmail.com>
2021-11-14 15:16:37 +0530

    viewdetail

--- src/component/frontPage.js
+++ src/component/frontPage.js
@@ -2,10 +2,34 @@
 import "./frontpage.css"
 import {useEffect, useState} from "react"
 import axios from "axios"
+import * as React from 'react';
+import Box from '@mui/material/Box';
+import Button from '@mui/material/Button';
+import Typography from '@mui/material/Typography';
+import Modal from '@mui/material/Modal';
+
+const style = {
+    position: 'absolute',
+    top: '50%',
+    left: '50%',
+    transform: 'translate(-50%, -50%)',
+    width: 400,
+    bgcolor: 'background.paper',
+    border: '2px solid #000',
+    boxShadow: 24,
+    p: 4,
+  };
+
 export function Front(){
         const [data,setData]=useState([])
         const [search,setSearch]=useState("")
         const [searchtitle,setSearchTitle]=useState("")
+
+
+        const [open, setOpen] = React.useState(false);
+        const handleOpen = () => setOpen(true);
+        const handleClose = () => setOpen(false);
+      
 
        useEffect(()=>{
            axios.get("http://localhost:8000/job").then((data)=>{
@@ -76,10 +100,31 @@ export function Front(){
                         <div style={{marginLeft:"10px"}}>
                         <p>{e.title}</p>
                         <p>{e.description}</p>
-                        <button className="viewDetail" style={{float:"right"}}>View Detail</button>
+                        <button onClick={()=>{
+                            
+                        }} className="viewDetail" style={{float:"right"}}>View Detail</button>
                         </div>
                     </div>)}
             </div>
+
+            <div>
+      <Button onClick={handleOpen}>Open modal</Button>
+      <Modal
+        open={open}
+        onClose={handleClose}
+        aria-labelledby="modal-modal-title"
+        aria-describedby="modal-modal-description"
+      >
+        <Box sx={style}>
+          <Typography id="modal-modal-title" variant="h6" component="h2">
+            Text in a modal
+          </Typography>
+          <Typography id="modal-modal-description" sx={{ mt: 2 }}>
+            Duis mollis, est non commodo luctus, nisi erat porttitor ligula.
+          </Typography>
+        </Box>
+      </Modal>
+    </div>
 
         </div>
 }
